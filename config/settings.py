f17979bc31767bc18a8f782b5191b476af4204e3
atabekdemurtaza <mychannel21war@gmail.com>
2023-04-23 20:25:23 +0500

    Added GrapqQL 23-04-2023

--- config/settings.py
+++ config/settings.py
@@ -127,8 +127,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'ratings/static'),)
+
+MEDIA_URL = 'ratings/media/'
+MEDIAR_ROOT = os.path.join(BASE_DIR, 'ratings/media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+GRAPHENE = {
+    'SCHEMA': 'scoops.schema.schema'
+}
--- scoops/admin.py
+++ scoops/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import (
+    User,
+    Book,
+    Comment
+)
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Book)
+admin.site.register(Comment)
